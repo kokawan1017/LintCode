76c7961601afe32c8b053079ad63c57126345b98
wanxiang <kokawan1017@hotmail.com>
2017-06-28 19:14:37 +0800

    斐波那契数列 优化二分查找

---
+++ Fibonacci/Fibonacci.cpp
@@ -0,0 +1,62 @@
+// Fibonacci.cpp : 定义控制台应用程序的入口点。
+//
+/*
+Find the Nth number in Fibonacci sequence.
+
+A Fibonacci sequence is defined as follow:
+
+The first two numbers are 0 and 1.
+The i th number is the sum of i-1 th number and i-2 th number.
+The first ten numbers in Fibonacci sequence is:
+
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34 ...
+
+Notice
+
+The Nth fibonacci number won't exceed the max value of signed 32-bit integer in the test cases.
+
+Example
+Given 1, return 0
+
+Given 2, return 1
+
+Given 10, return 34
+*/
+
+#include "stdafx.h"
+#include <iostream>
+
+/*
+斐波那契数列: 迭代解法
+*/
+int fibonacci(int n)
+{
+    if (n == 1)
+    {
+        return 0;
+    }
+    if (n == 2)
+    {
+        return 1;
+    }
+
+    int a = 0;
+    int b = 1;
+    for (int i = 3; i <= n; ++i)
+    {
+        int tmp = b;
+        b = a + b;
+        a = tmp;
+    }
+
+    return b;
+}
+
+int main()
+{
+    std::cout << fibonacci(10) << std::endl;
+
+    std::cout << fibonacci(41) << std::endl;
+    return 0;
+}
+
--- FirstPositionOfTarget/FirstPositionOfTarget.cpp
+++ FirstPositionOfTarget/FirstPositionOfTarget.cpp
@@ -9,7 +9,6 @@ For a given sorted array (ascending order) and a target number, find the first i
 
 If the target number does not exist in the array, return -1.
 
-Have you met this question in a real interview? Yes
 Example
 If the array is [1, 2, 3, 3, 4, 5, 10], for given target 3, return 2.
 
@@ -29,29 +28,28 @@ int search(std::vector<int>& array, int target, int start, int end)
         return -1;
     }
 
-    int mid = (end - start) / 2;
+    int mid = start + (end - start) / 2;
 
     if (array[mid] == target)
     {
-        while (mid != start)
+        int pos = search(array, target, start, mid);
+        if (pos == -1)
         {
-            --mid;
-            if (array[mid] < target)
-            {
-                ++mid;
-                break;
-            }
+            return mid;
         }
-        return mid;
+
+        return pos;
     }
 
     if (array[mid] < target)
     {
-        return search(array, target, mid + 1, end);
+        int pos = search(array, target, mid + 1, end);
+        return pos;
     }
     else if (target < array[mid])
     {
-        return search(array, target, start, mid);
+        int pos = search(array, target, start, mid);
+        return pos;
     }
 
     return -1;
@@ -69,6 +67,14 @@ int main()
     std::vector<int> nums = { 1, 2, 3, 3, 4, 5, 10 };
 
     std::cout << binarySearch(nums, 3) << std::endl;
+
+    std::vector<int> n2 = { 2,6,8,13,15,17,17,18,19,20 };
+
+    std::cout << binarySearch(n2, 15) << std::endl;
+
+    std::vector<int> n3 = { 3,4,5,8,8,8,8,10,13,14 };
+
+    std::cout << binarySearch(n3, 8) << std::endl;
 
     return 0;
 }
