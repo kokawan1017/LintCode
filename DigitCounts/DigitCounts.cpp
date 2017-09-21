0e6bd0642c5f83b298e0e3b8fde537db0814f585
wanxiang <wanxiang@WANXIANG-PC>
2017-09-21 16:38:23 +0800

    没什么

--- DigitCounts/DigitCounts.cpp
+++ DigitCounts/DigitCounts.cpp
@@ -3,6 +3,19 @@
 
 #include "stdafx.h"
 
+int countOne(int n, int k)
+{
+    int count = 0;
+    while (n > 0)
+    {
+        if (n % 10 == k)
+        {
+            ++count;
+        }
+        n /= 10;
+    }
+}
+
 int digitCounts(int k, int n)
 {
     // write your code here
@@ -12,4 +25,3 @@ int main()
 {
     return 0;
 }
-
